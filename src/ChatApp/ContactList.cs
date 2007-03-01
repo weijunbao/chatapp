46f3b6fa8b23ac46f066e29d65426e8f14687281
georgeck <georgeck@b93edf16-a12e-0410-8d94-15edfff5a51b>
2007-03-01 16:50:40 +0000

    =added a comment=

--- src/ChatApp/ContactList.cs
+++ src/ChatApp/ContactList.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using Coversant.SoapBox.Base;
-
+//this is contact list
 namespace ChatApp
 {
     public class ContactList
